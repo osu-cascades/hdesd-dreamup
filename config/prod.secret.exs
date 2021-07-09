11d9f5640764f12fc4a641610fa32bb4d673f614
Ethan Barker <ethanbarker0@gmail.com>
2021-07-09 16:24:18 -0700

    Preparing for heroku deploy

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :dream_up, DreamUp.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
