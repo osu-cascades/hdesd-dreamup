d9f3b942cf869631e114d3795caf791c4ad35276
Ian Snyder <isnide23@gmail.com>
2021-10-13 17:03:13 -0700

    add award cards to database and to awards page References #19

--- lib/dream_up_web/live/awards_live.ex
+++ lib/dream_up_web/live/awards_live.ex
@@ -7,15 +7,17 @@ defmodule DreamUpWeb.AwardsLive do
   alias DreamUp.Cards
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, foo: nil)
+    socket = assign(socket, cards: Cards.list_cards())
     {:ok, socket}
   end
 
   def handle_params(params, _url, socket) do
-    IO.inspect(params["game_id"])
-    IO.inspect(params["player_id"])
-    {:noreply, socket}
+    {:noreply, assign(socket, game: Games.get_game!(params["game_id"]), player: Players.get_player!(params["player_id"]))}
   end
+
+
+
+
 
 
 end
