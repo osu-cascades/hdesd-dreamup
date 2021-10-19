4853e8b6d5c7fcbebd1ea6d7c7db54b6c1b2a248
Amber Kolar <amberj.kolar@gmail.com>
2021-10-19 16:00:10 -0700

    Let team leader choose awards for other team, display all awarded awards for each team separately References #19

--- lib/dream_up_web/live/awards_live.ex
+++ lib/dream_up_web/live/awards_live.ex
@@ -5,9 +5,10 @@ defmodule DreamUpWeb.AwardsLive do
   alias DreamUp.Games
   alias DreamUp.Players
   alias DreamUp.Cards
+  alias DreamUp.Awards
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, cards: Cards.list_cards())
+    socket = assign(socket, cards: Cards.list_cards(), awards: Awards.list_awards())
     {:ok, socket}
   end
 
@@ -15,9 +16,14 @@ defmodule DreamUpWeb.AwardsLive do
     {:noreply, assign(socket, game: Games.get_game!(params["game_id"]), player: Players.get_player!(params["player_id"]))}
   end
 
-
-
-
-
+  def handle_event("award-click", %{"card-id" => card_id}, socket) do
+    case socket.assigns.player.team do
+      "red" ->
+        Awards.create_award(%{game_id: socket.assigns.game.id, team: "blue", card_id: String.to_integer(card_id)})
+      "blue" ->
+        Awards.create_award(%{game_id: socket.assigns.game.id, team: "red", card_id: String.to_integer(card_id)})
+    end
+    {:noreply, socket}
+  end
 
 end
