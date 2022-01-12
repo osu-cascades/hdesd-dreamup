cadc727bd39fe7796e87b63a1cbf3a272a7b523c
Amber Kolar <amberj.kolar@gmail.com>
2022-01-11 16:39:17 -0800

    Remove 'next' and 'back' event handlers from instructions_live.ex References #24

--- lib/dream_up_web/live/instructions_live.ex
+++ lib/dream_up_web/live/instructions_live.ex
@@ -5,23 +5,7 @@ defmodule DreamUpWeb.InstructionsLive do
   @max_step 6
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, step: 1)}
-  end
-
-  def handle_event("back", _, socket) do
-    if socket.assigns.step == 1 do
-      {:noreply, redirect(socket, to: Routes.home_path(socket, :index))}
-    else
-      {:noreply, assign(socket, step: socket.assigns.step - 1)}
-    end
-  end
-
-  def handle_event("next", _, socket) do
-    if socket.assigns.step == @max_step do
-      {:noreply, redirect(socket, to: Routes.home_path(socket, :index))}
-    else
-      {:noreply, assign(socket, step: socket.assigns.step + 1)}
-    end
+    {:ok, socket}
   end
 
 end
