021210bbe3d07c1981b9ba706d252d554ae93744
Ian Snyder <isnide23@gmail.com>
2022-01-31 13:36:42 -0800

    style landing and route buttons References #25

--- lib/dream_up_web/live/home_live.ex
+++ lib/dream_up_web/live/home_live.ex
@@ -2,8 +2,6 @@ defmodule DreamUpWeb.HomeLive do
 
   use DreamUpWeb, :live_view
 
-  # alias DreamUp.Games
-
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
@@ -11,5 +9,13 @@ defmodule DreamUpWeb.HomeLive do
   # def handle_params(params, _url, socket) do
   #   {:ok, socket}
   # end
+
+  def handle_event("start-playing", _, socket) do
+    {:noreply, redirect(socket, to: Routes.live_path(socket, DreamUpWeb.StartLive))}
+  end
+
+  def handle_event("view-instructions", _, socket) do
+    {:noreply, redirect(socket, to: Routes.live_path(socket, DreamUpWeb.InstructionsLive))}
+  end
 
 end
--- lib/dream_up_web/live/instructions_live.ex
+++ lib/dream_up_web/live/instructions_live.ex
@@ -2,8 +2,6 @@ defmodule DreamUpWeb.InstructionsLive do
 
   use DreamUpWeb, :live_view
 
-  @max_step 6
-
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
